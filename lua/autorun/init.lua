dcccffb4e986afcb1308edae2c3af82de53a727b
tearbyte <bite.a.tear@gmail.com>
2025-08-24 20:45:41 +0300

    Update loader

--- lua/autorun/init.lua
+++ lua/autorun/init.lua
@@ -1,34 +1,58 @@
+local function load(path, client)
+	if CLIENT and not client then return end
+	if SERVER and client then AddCSLuaFile( 'teralib/' .. path .. '.lua' ) end
+	if SERVER and path:find('client') then return end
+	include( 'teralib/' .. path .. '.lua' )
+end
+
+local function ljust(str, length, padChar)
+	padChar = padChar or ' '
+	return str .. string.rep(padChar, math.max(0, length - #str))
+end
+
+local client_patches = {
+	'globals',
+	'lang',
+	'net',
+	'vgui',
+}
+
+local server_patches = {
+	'globals',
+	'net'
+}
+
+local shared_patches = {
+	'enums',
+	'tables',
+	'util'
+}
+
+print( '/¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ TeraLib Loading ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\\')
 if SERVER then
 	resource.AddWorkshop( '3013984464' )
-	print('{TeraLib initiating serverside...}')
-	print('{Loading SERVERSIDE files...}')
-	for k, v in pairs(file.Find('teralib/server/*', 'LUA')) do
-		include('teralib/server/' .. v)
-		print(v)
+
+	print( '| Loading SERVERSIDE files...                       |' )
+
+	for k, v in pairs( server_patches ) do
+		load( 'server/' .. v, false )
+		print( '| ' .. ljust( 'server/' .. v .. '.lua', 50 ) .. '|' )
 	end
-	print('{Loading SHARED files...}')
-	for k, v in pairs(file.Find('teralib/*', 'LUA')) do
-		include('teralib/' .. v)
-		AddCSLuaFile('teralib/' .. v)
-		print(v)
-	end
-	print('{Sending CLIENT files to clientside...}')
-	for k, v in pairs(file.Find('teralib/client/*', 'LUA')) do
-		AddCSLuaFile('teralib/client/' .. v)
-		print(v)
-	end
-	print('{TeraLib has done loading!}')
-else
-	print('{TeraLib initiating clientside...}')
-	print('{Loading CLIENTSIDE files...}')
-	for k, v in pairs(file.Find('teralib/client/*', 'LUA')) do
-		include('teralib/client/' .. v)
-		print(v)
-	end
-	print('{Loading SHARED files...}')
-	for k, v in pairs(file.Find('teralib/*', 'LUA')) do
-		include('teralib/' .. v)
-		print(v)
-	end
-	print('{TeraLib has done loading!}')
+
+	print( '|                                                   |')
 end
+
+print( '| ' .. ( CLIENT and 'Loading' or 'Sending' ) ..' CLIENTSIDE files...                       |' )
+for k, v in pairs( client_patches ) do
+	load( 'client/' .. v, true )
+	print( '| ' .. ljust( 'client/' .. v .. '.lua', 50 ) .. '|' )
+end
+
+print( '|                                                   |')
+print( '| Loading SHARED files...                           |' )
+for k, v in pairs( shared_patches ) do
+	load( v, true )
+	print( '| ' .. ljust( v .. '.lua', 50 ) .. '|' )
+end
+
+print( '\\_________________ TeraLib ready! __________________/')
